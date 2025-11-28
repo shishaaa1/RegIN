d42d41f570f60d26cd215314fd4b74ea3aa4e74b
DanilGashev <lalalamistikc31@gmail.com>
2025-11-28 19:51:13 +0500

    Разработанный класс WorkingDB

---
+++ Classes/WorkingDB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace RegIN.Classes
+{
+    public class WorkingDB
+    {
+        readonly static string connection = "server=localhost;port=3306;database=regin;user=root;pwd=root";
+        public static MySqlConnection OpenConnection()
+        {
+            try {
+                MySqlConnection mySqlConnection = new MySqlConnection(connection);
+                mySqlConnection.Open();
+                return mySqlConnection;
+            }
+            catch(Exception exp)
+            {
+                Debug.WriteLine(exp.Message);
+                return null;
+            }
+        }
+        public static MySqlDataReader Query(string Sql, MySqlConnection mySqlConnection)
+        {
+            MySqlCommand mySqlCommand = new MySqlCommand(Sql,mySqlConnection);
+            return mySqlCommand.ExecuteReader();
+        }
+        public static void CloseConnection(MySqlConnection mySqlConnection)
+        {
+            mySqlConnection.Close();
+            MySqlConnection.ClearPool(mySqlConnection);
+        }
+        public static bool OpenConnection(MySqlConnection mySqlConnection)
+        {
+            return mySqlConnection != null && mySqlConnection.State == System.Data.ConnectionState.Open;
+        }
+    }
+    
+}
